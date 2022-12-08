49cc1d084b5de6a28017ac676bdc87dfbcc1298f
Svante Lidman <svante.lidman@gmail.com>
2022-12-08 19:33:07 +0100

    Rust fmt:ed

--- d8/src/main.rs
+++ d8/src/main.rs
@@ -20,28 +20,49 @@ fn count_visible(trees: &Vec<Vec<usize>>) -> usize {
     let tree_cols = as_cols(trees);
     let n_cols = trees[0].len();
     let n_rows = trees.len();
-    (0..n_rows).into_iter().map(|ir| (0..n_cols).into_iter().filter(|ic| is_visible(trees, &tree_cols, ir, *ic)).count()).sum()
+    (0..n_rows)
+        .into_iter()
+        .map(|ir| {
+            (0..n_cols)
+                .into_iter()
+                .filter(|ic| is_visible(trees, &tree_cols, ir, *ic))
+                .count()
+        })
+        .sum()
 }
 
 fn highest_scenic_score(trees: &Vec<Vec<usize>>) -> usize {
     let tree_cols = as_cols(trees);
     let n_rows = trees.len();
     let n_cols = trees[0].len();
-    iproduct!(0..n_rows, 0..n_cols).into_iter().map(|(ic, ir)| scenic_score(trees, &tree_cols, ir, ic)).max().unwrap()
+    iproduct!(0..n_rows, 0..n_cols)
+        .into_iter()
+        .map(|(ic, ir)| scenic_score(trees, &tree_cols, ir, ic))
+        .max()
+        .unwrap()
 }
 
 fn in_outer_line_of_sight(trees: &Vec<usize>, ind: usize) -> bool {
     if ind == 0 || ind == trees.len() - 1 {
         true
     } else {
-        *trees[0..ind].iter().max().unwrap() < trees[ind] || *trees[ind + 1..].iter().max().unwrap() < trees[ind]
+        *trees[0..ind].iter().max().unwrap() < trees[ind]
+            || *trees[ind + 1..].iter().max().unwrap() < trees[ind]
     }
 }
 
 fn as_cols(rows: &Vec<Vec<usize>>) -> Vec<Vec<usize>> {
     let n_cols = rows[0].len();
     let n_rows = rows.len();
-    (0..n_cols).into_iter().map(|ic| (0..n_rows).into_iter().map(|ir| rows[ir][ic]).collect::<Vec<_>>()).collect::<Vec<_>>()
+    (0..n_cols)
+        .into_iter()
+        .map(|ic| {
+            (0..n_rows)
+                .into_iter()
+                .map(|ir| rows[ir][ic])
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>()
 }
 
 fn is_visible(rows: &Vec<Vec<usize>>, cols: &Vec<Vec<usize>>, row: usize, col: usize) -> bool {
@@ -53,11 +74,19 @@ fn scenic_score(rows: &Vec<Vec<usize>>, cols: &Vec<Vec<usize>>, row: usize, col:
 }
 
 fn line_of_sight_score(trees: &Vec<usize>, ind: usize) -> usize {
-    let upper_length =  trees.len() - ind - 1;
-    let lower_length =  ind;
-    let hi_score = if let Some(pos) = trees[ind+1..].iter().position(|t| t >= &trees[ind]) {pos+1} else { upper_length };
-    let lo_score = if let Some(pos) = trees[0..ind].iter().rev().position(|t| t >= &trees[ind]) {pos+1} else { lower_length };
-    hi_score*lo_score
+    let upper_length = trees.len() - ind - 1;
+    let lower_length = ind;
+    let hi_score = if let Some(pos) = trees[ind + 1..].iter().position(|t| t >= &trees[ind]) {
+        pos + 1
+    } else {
+        upper_length
+    };
+    let lo_score = if let Some(pos) = trees[0..ind].iter().rev().position(|t| t >= &trees[ind]) {
+        pos + 1
+    } else {
+        lower_length
+    };
+    hi_score * lo_score
 }
 
 #[cfg(test)]
